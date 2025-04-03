41aeab299ada05a09d1c41bee570c6c1b789a436
ajiayi-debug <e0774928@u.nus.edu>
2025-04-03 23:52:16 +0800

    give cot dataset

--- dataset_creation/data.py
+++ dataset_creation/data.py
@@ -2,13 +2,16 @@ import pandas as pd
 from datasets import load_dataset
 
 ds = load_dataset("Fahad-S/videobench_cotv01")
+ds = ds["test"]
 
-df = pd.read_parquet("hf://datasets/lmms-lab/VideoChatGPT/Generic/test-00000-of-00001.parquet")
-df.to_csv('data/generic.csv')
+ds.to_csv("data/cot.csv")
 
-df = pd.read_parquet("hf://datasets/lmms-lab/VideoChatGPT/Consistency/test-00000-of-00001.parquet")
-df.to_csv('data/consistency.csv')
+# df = pd.read_parquet("hf://datasets/lmms-lab/VideoChatGPT/Generic/test-00000-of-00001.parquet")
+# df.to_csv('data/generic.csv')
 
-df = pd.read_parquet("hf://datasets/lmms-lab/VideoChatGPT/Temporal/test-00000-of-00001.parquet")
-df.to_csv('data/temporal.csv')
+# df = pd.read_parquet("hf://datasets/lmms-lab/VideoChatGPT/Consistency/test-00000-of-00001.parquet")
+# df.to_csv('data/consistency.csv')
+
+# df = pd.read_parquet("hf://datasets/lmms-lab/VideoChatGPT/Temporal/test-00000-of-00001.parquet")
+# df.to_csv('data/temporal.csv')
 
