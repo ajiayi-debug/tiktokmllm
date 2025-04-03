f9c25af1d0dda54195955cae21b253c73c73062f
Jia Yi <jiayi.ang@gxs.com.sg>
2025-04-03 23:47:02 +0800

    load cot dataset

--- dataset_creation/data.py
+++ dataset_creation/data.py
@@ -1,4 +1,7 @@
 import pandas as pd
+from datasets import load_dataset
+
+ds = load_dataset("Fahad-S/videobench_cotv01")
 
 df = pd.read_parquet("hf://datasets/lmms-lab/VideoChatGPT/Generic/test-00000-of-00001.parquet")
 df.to_csv('data/generic.csv')
@@ -8,3 +11,4 @@ df.to_csv('data/consistency.csv')
 
 df = pd.read_parquet("hf://datasets/lmms-lab/VideoChatGPT/Temporal/test-00000-of-00001.parquet")
 df.to_csv('data/temporal.csv')
+
